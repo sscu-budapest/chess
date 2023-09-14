3e882c5d962f8285e51f8c12a3efe4d02e62bf83
Endre Márk Borza <endremborza@gmail.com>
2023-09-14 18:57:44 +0200

    add lichess collection

---
+++ src/lichess.py
@@ -0,0 +1,32 @@
+import aswan
+import datazimmer as dz
+
+main_url = dz.SourceUrl("https://lichess.org/api/tournament")
+
+
+class GetTournaments(aswan.RequestJsonHandler):
+    def parse(self, obj: dict):
+        finished = obj["finished"]
+        urls = [
+            aswan.add_url_params(
+                f"{main_url}/{d['id']}/games",
+                params={"clocks": True, "accuracy": True},
+            )
+            for d in finished
+        ]
+        self.register_links_to_handler(urls, GetTournamentGames)
+        return finished
+
+
+class GetTournamentGames(aswan.RequestHandler):
+    process_indefinitely = True
+    max_in_parallel = 1
+
+    def parse(self, blob):
+        return blob
+
+
+class LichessDza(dz.DzAswan):
+    name: str = "lichess"
+    cron: str = "*/30 * * * *"
+    starters = {GetTournaments: [main_url]}
